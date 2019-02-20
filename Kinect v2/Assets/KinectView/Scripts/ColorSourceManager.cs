aa38849841a3097d928e8d67130b659740900cda
Anurag Syal <syal.anurag1991@gmail.com>
2019-02-19 16:19:59 -0800

    Basic working library for interfacing Kinect V2 with Unity

---
+++ Kinect v2/Assets/KinectView/Scripts/ColorSourceManager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+using UnityEngine.UI;
+
+public class ColorSourceManager : MonoBehaviour 
+{
+	public RawImage ColorView;
+
+    public int ColorWidth { get; private set; }
+    public int ColorHeight { get; private set; }
+	public uint BytesPerPixel { get; private set; }
+
+    private KinectSensor _Sensor;
+    private ColorFrameReader _Reader;
+    private byte[] _Data;
+	private long _TotalPixelData;
+
+	private Texture2D _Texture;
+	public Texture2D GetColorTexture()
+    {
+        return _Texture;
+    }
+
+	public byte GetPixelColorByPosition(float x, float y, int rgbaOrder)
+	{
+		int x_index = (int)(x + 0.5f);
+		int y_index = (int)(y + 0.5f);
+		if (x_index < ColorWidth && x_index >= 0 && y_index < ColorHeight && y_index >= 0)
+			return _Data[(BytesPerPixel * (ColorWidth * y_index + x_index)) + rgbaOrder];
+		return 0;
+	}
+
+	void Start()
+    {
+        _Sensor = KinectSensor.GetDefault();
+        
+        if (_Sensor != null) 
+        {
+            _Reader = _Sensor.ColorFrameSource.OpenReader();
+            
+            var frameDesc = _Sensor.ColorFrameSource.CreateFrameDescription(ColorImageFormat.Rgba);
+            ColorWidth = frameDesc.Width;
+            ColorHeight = frameDesc.Height;
+			BytesPerPixel = frameDesc.BytesPerPixel;
+			_TotalPixelData = BytesPerPixel * ColorWidth * ColorHeight;
+
+            _Texture = new Texture2D(frameDesc.Width, frameDesc.Height, TextureFormat.RGBA32, false);
+            _Data = new byte[frameDesc.BytesPerPixel * frameDesc.LengthInPixels];
+            
+            if (!_Sensor.IsOpen)
+                _Sensor.Open();
+        }
+    }
+    
+    void Update () 
+    {
+        if (_Reader != null) 
+        {
+            var frame = _Reader.AcquireLatestFrame();
+            if (frame != null)
+            {
+                frame.CopyConvertedFrameDataToArray(_Data, ColorImageFormat.Rgba);
+                _Texture.LoadRawTextureData(_Data);
+                _Texture.Apply();
+				if (ColorView != null)
+					ColorView.texture = _Texture;
+
+                frame.Dispose();
+                frame = null;
+            }
+		}
+	}
+
+    void OnApplicationQuit()
+    {
+        if (_Reader != null) 
+        {
+            _Reader.Dispose();
+            _Reader = null;
+        }
+        
+        if (_Sensor != null) 
+        {
+            if (_Sensor.IsOpen)
+                _Sensor.Close();
+            _Sensor = null;
+        }
+    }
+}
---
+++ Kinect v2/Assets/RegisteredColorView.cs
@@ -0,0 +1,49 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Windows.Kinect;
+
+public class RegisteredColorView : MonoBehaviour
+{
+	public GameObject ColorSourceManager;
+	private ColorSourceManager _ColorManager;
+	private MultiSourceManager _MultiManager;
+	private Renderer objectRenderer;
+
+	void Start()
+	{
+		objectRenderer = gameObject.GetComponent<Renderer>();
+		if (objectRenderer != null)
+			objectRenderer.material.SetTextureScale("_MainTex", new Vector2(-1, 1));
+	}
+
+	void Update()
+	{
+		if (ColorSourceManager == null)
+		{
+			return;
+		}
+
+		_ColorManager = ColorSourceManager.GetComponent<ColorSourceManager>();
+		if (_ColorManager != null)
+		{
+			if (objectRenderer != null)
+				gameObject.GetComponent<Renderer>().material.mainTexture = _ColorManager.GetColorTexture();
+			else
+				gameObject.GetComponent<RawImage>().texture = _ColorManager.GetColorTexture();
+			return;
+		}
+
+		_MultiManager = ColorSourceManager.GetComponent<MultiSourceManager>();
+		if (_MultiManager != null)
+		{
+			if (objectRenderer != null)
+				gameObject.GetComponent<Renderer>().material.mainTexture = _MultiManager.GetColorTexture();
+			else
+				gameObject.GetComponent<RawImage>().texture = _MultiManager.GetColorTexture();
+		}
+
+		return;
+	}
+}
